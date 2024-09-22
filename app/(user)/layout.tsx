bef7df119d0b12decb4262dda63f2af8766ad8ab
kn211_bmo <kn211_bmo@student.ztu.edu.ua>
2024-09-23 01:13:55 +0300

    feat: ws base url

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,12 +1,12 @@
 import { ChatContextProvider } from "@/components/shared/ChatContext";
 import { SocketProvider } from "@/components/shared/SocketProvider";
 import { UserContextProvider } from "@/components/shared/UserContext";
-import { API_BASE_URL } from "@/utils/constants";
+import { API_BASE_URL, WS_BASE_URL } from "@/utils/constants";
 import { PropsWithChildren } from "react";
 const UserLayout = ({ children }: PropsWithChildren) => {
   return (
     <UserContextProvider>
-      <SocketProvider url={API_BASE_URL}>
+      <SocketProvider url={WS_BASE_URL}>
         <ChatContextProvider>{children}</ChatContextProvider>
       </SocketProvider>
     </UserContextProvider>
--- utils/constants.ts
+++ utils/constants.ts
@@ -1,4 +1,7 @@
 export const API_BASE_URL =
   process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8000/api";
 
+export const WS_BASE_URL =
+  process.env.NEXT_PUBLIC_WS_BASE_URL || "http://localhost:8000";
+
 export const authTokenName = "authToken";

856bc18ec3860025aab75f892741921495dd0479
kn211_bmo <kn211_bmo@student.ztu.edu.ua>
2024-09-23 01:03:03 +0300

    fix: socket url error

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,11 +1,12 @@
 import { ChatContextProvider } from "@/components/shared/ChatContext";
 import { SocketProvider } from "@/components/shared/SocketProvider";
 import { UserContextProvider } from "@/components/shared/UserContext";
+import { API_BASE_URL } from "@/utils/constants";
 import { PropsWithChildren } from "react";
 const UserLayout = ({ children }: PropsWithChildren) => {
   return (
     <UserContextProvider>
-      <SocketProvider url="http://localhost:8000">
+      <SocketProvider url={API_BASE_URL}>
         <ChatContextProvider>{children}</ChatContextProvider>
       </SocketProvider>
     </UserContextProvider>

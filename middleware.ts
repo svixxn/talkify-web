364e1468465d7f0fa862e151b14f40ad772d7f5c
Maksym Bakovetskyi <bakoveckiymax@gmail.com>
2025-01-26 14:58:17 +0200

    fix: redirect user to chat if he is already logged in

--- middleware.ts
+++ middleware.ts
@@ -2,13 +2,15 @@ import { NextRequest, NextResponse } from "next/server";
 import { authTokenName } from "./utils/constants";
 
 export const config = {
-  matcher: ["/chat"],
+  matcher: ["/chat", "/"],
 };
 
 export function middleware(request: NextRequest) {
   const tokenCookie = request.cookies.get(authTokenName);
   if (tokenCookie && tokenCookie.value && tokenCookie.value !== "deleted") {
-    return NextResponse.next();
+    if (request.nextUrl.pathname === "/")
+      return NextResponse.redirect(new URL("/chat", request.url));
+    else return NextResponse.next();
   } else {
     return NextResponse.redirect(new URL("/", request.url));
   }
